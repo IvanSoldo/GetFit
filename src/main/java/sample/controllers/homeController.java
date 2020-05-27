688890fb36bf85794887104df829b018c5f469ff
IvanSoldo <65110340+IvanSoldo@users.noreply.github.com>
2020-05-27 16:01:32 +0200

    Before Form validation

--- src/main/java/sample/controllers/homeController.java
+++ src/main/java/sample/controllers/homeController.java
@@ -1,6 +1,8 @@
 package sample.controllers;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,11 +10,25 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import sample.models.Food;
+import sample.repositories.FoodRepository;
+import sample.repositories.FoodRepositoryImpl;
+import sample.service.FoodService;
+import sample.service.FoodServiceImpl;
 import sample.utilities.ApplicationState;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class HomeController {
-    ApplicationState applicationState = new ApplicationState();
+    private ApplicationState applicationState = new ApplicationState();
+    private FoodRepository foodRepository = new FoodRepositoryImpl();
+    private FoodService foodService = new FoodServiceImpl();
+    private ArrayList<Food> foods = new ArrayList<>();
 
 
     @FXML
@@ -35,18 +51,62 @@ public class HomeController {
     private Label remainingLabel;
 
     @FXML
+    private Label macrosRemLabel;
+
+    @FXML
+    private TableView<Food> foodTable;
+
+    @FXML
+    private TableColumn<Food, String> columnName;
+
+    @FXML
+    private TableColumn<Food, Integer> columnCalories;
+
+    @FXML
+    private TableColumn<Food, Integer> columnProteins;
+
+    @FXML
+    private TableColumn<Food, Integer> columnCarbs;
+
+    @FXML
+    private TableColumn<Food, Integer> columnFats;
+
+    @FXML
+    private TableColumn<Food, Integer> columnAmount;
+
+    @FXML
     private void initialize() {
+        foods = foodRepository.getTotalFoodsFromDb();
         usernameLabel.setText(applicationState.getAccount().getUsername());
         caloriesLabel.setText(String.valueOf(applicationState.getAccount().getCalories().getCalories()));
         String proteins = String.valueOf(applicationState.getAccount().getMacros().getProteins());
         String carbs = String.valueOf(applicationState.getAccount().getMacros().getCarbs());
         String fats = String.valueOf(applicationState.getAccount().getMacros().getFats());
         macrosGoalLabel.setText("P:" + proteins + " C:" + carbs + " F:" + fats);
+
         calCalcLabel.setText(String.valueOf(applicationState.getAccount().getCalories().getCalories()));
         remainingLabel.setText(String.valueOf(applicationState.getRemainingCalories().getCalories()));
-        // privremeno dok ne stavim total_food tablicu
-        int food = applicationState.getAccount().getCalories().getCalories() - applicationState.getRemainingCalories().getCalories();
-        foodLabel.setText(String.valueOf(food));
+
+        String remProteins = String.valueOf(applicationState.getRemainingCalories().getProteins());
+        String remCarbs = String.valueOf(applicationState.getRemainingCalories().getCarbs());
+        String remFats = String.valueOf(applicationState.getRemainingCalories().getFats());
+
+        macrosRemLabel.setText("P:" + remProteins + " C:" + remCarbs + " F:" + remFats);
+        int foodsCal = foodService.calculateTotalFoodCalories(foods);
+        foodLabel.setText(String.valueOf(foodsCal));
+
+
+
+        columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        columnCalories.setCellValueFactory(new PropertyValueFactory<>("calories"));
+        columnProteins.setCellValueFactory(new PropertyValueFactory<>("proteins"));
+        columnCarbs.setCellValueFactory(new PropertyValueFactory<>("carbs"));
+        columnFats.setCellValueFactory(new PropertyValueFactory<>("fats"));
+        columnAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        foodTable.setPlaceholder(new Label("No food added today"));
+
+        ObservableList<Food> observableList = FXCollections.observableArrayList(foods);
+        foodTable.setItems(observableList);
 
     }
 
@@ -71,7 +131,22 @@ public class HomeController {
     }
 
     @FXML
-    void customizeCaloriesAndMacrosClick(ActionEvent event) {
+    void customizeCaloriesAndMacrosClick(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/customizeView.fxml"));
+        Scene scene = new Scene(root);
 
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    void logOutButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/loginView.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
     }
 }
